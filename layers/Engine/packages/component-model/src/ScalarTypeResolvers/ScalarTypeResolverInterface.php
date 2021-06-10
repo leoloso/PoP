511dc0e57541164e350bcc476d74481682dee32e
Leonardo Losoviz <leo@getpop.org>
2021-06-10 14:47:13 +0800

    Improved doc

--- layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
@@ -14,21 +14,27 @@ interface ScalarTypeResolverInterface
     public function getScalarName(): string;
 
     /**
-     * Result coercion
+     * Result coercion. Called by the (GraphQL) engine when printing the response.
+     *
+     * It takes the scalar entity as an input (an object, string, int, etc),
+     * and it is converted into a string, to be output on the response.
      *
      * @return string formatted representation of the custom scalar as a string
      */
     public function serialize(mixed $scalarValue): string;
 
     /**
-     * Literal input coercion
+     * Literal input coercion. Called by the (GraphQL) engine to convert an input
+     * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
+     * into the corresponding scalar entity (in this case, a String).
      *
      * @return mixed the (custom) scalar
      */
     public function parseLiteral(mixed $inputValue): mixed;
 
     /**
-     * Value input coercion
+     * Value input coercion. Similar to `serialize`, but returning the scalar entity
+     * instead of a string
      *
      * @return mixed the (custom) scalar
      */
