35c7c32fc767a19aff257b3c825a9c812d3f927b
Leonardo Losoviz <leo@getpop.org>
2021-06-10 14:58:10 +0800

    Improved doc and types

--- layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
@@ -19,22 +19,29 @@ interface ScalarTypeResolverInterface
      * It takes the scalar entity as an input (an object, string, int, etc),
      * and it is converted into a string, to be output on the response.
      *
-     * @return string|int formatted representation of the custom scalar
+     * @return string|int|bool|float formatted representation of the custom scalar
      */
-    public function serialize(mixed $scalarValue): string|int;
+    public function serialize(mixed $scalarValue): string|int|bool|float;
 
     /**
      * Literal input coercion. Called by the (GraphQL) engine to convert an input
      * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
      * into the corresponding scalar entity (in this case, a String).
+     * 
+     * Using `string|int|bool|float` as input types, since those are all possible
+     * types that can be used as inputs.
      *
      * @return mixed the (custom) scalar
      */
-    public function parseLiteral(mixed $inputValue): mixed;
+    public function parseLiteral(string|int|bool|float $inputValue): mixed;
 
     /**
-     * Value input coercion. Similar to `serialize`, but returning the scalar entity
-     * instead of a string
+     * Value input coercion. Similar to `parseLiteral` in that it:
+     * 
+     * - takes the representation of the scalar as input,
+     *   with the addition that it can also be an object
+     *   (that's why it receives `mixed`).
+     * - must return the scalar entity
      *
      * @return mixed the (custom) scalar
      */
