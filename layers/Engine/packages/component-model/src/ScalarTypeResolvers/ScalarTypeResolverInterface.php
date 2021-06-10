229358ffa090be11f1b551e5290720bcac2c0aa4
Leonardo Losoviz <leo@getpop.org>
2021-06-10 15:08:24 +0800

    Also allow `array` type

--- layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
+++ layers/Engine/packages/component-model/src/ScalarTypeResolvers/ScalarTypeResolverInterface.php
@@ -16,34 +16,34 @@ interface ScalarTypeResolverInterface
     /**
      * Result coercion. Called by the (GraphQL) engine when printing the response.
      *
-     * It takes the scalar entity as an input (an object, string, int, etc),
+     * It takes the scalar entity as an input (any type except `null`),
      * and it is converted into a string, to be output on the response.
      *
-     * @return string|int|bool|float formatted representation of the custom scalar
+     * @return string|int|float formatted representation of the custom scalar
      */
-    public function serialize(mixed $scalarValue): string|int|bool|float;
+    public function serialize(string|int|bool|float|object|array $scalarValue): string|int|float|array;
 
     /**
      * Literal input coercion. Called by the (GraphQL) engine to convert an input
      * (such as field argument `"Hallo!"` in `{ echo(msg: "Hallo!") }`)
      * into the corresponding scalar entity (in this case, a String).
      * 
-     * Using `string|int|bool|float` as input types, since those are all possible
+     * Using `string|int|float` as input types, since those are all possible
      * types that can be used as inputs.
      *
-     * @return mixed the (custom) scalar
+     * @return string|int|float|object|array the (custom) scalar
      */
-    public function parseLiteral(string|int|bool|float $inputValue): mixed;
+    public function parseLiteral(string|int|float|array $inputValue): string|int|bool|float|object|array;
 
     /**
-     * Value input coercion. Similar to `parseLiteral` in that it:
+     * Value input coercion.
      * 
-     * - takes the representation of the scalar as input,
-     *   with the addition that it can also be an object
-     *   (that's why it receives `mixed`).
-     * - must return the scalar entity
+     * Similar to `serialize` in that it can take any input: the (custom)
+     * scalar itself, or a representation of it (as string, int, etc).
+     * 
+     * Similar to `parseLiteral` in that it must return the scalar entity
      *
-     * @return mixed the (custom) scalar
+     * @return string|int|bool|float|object|array the (custom) scalar
      */
-    public function parseValue(mixed $scalarValue): mixed;
+    public function parseValue(string|int|bool|float|object|array $scalarValue): string|int|bool|float|object|array;
 }
