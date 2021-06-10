8e2514b6bff5e97aa3f1380d92e6125a47dfc7e6
Leonardo Losoviz <leo@getpop.org>
2021-06-10 15:20:09 +0800

    Added class

---
+++ layers/Engine/packages/component-model/src/ScalarTypeResolvers/AbstractCustomScalarTypeResolver.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\ScalarTypeResolvers;
+
+abstract class AbstractCustomScalarTypeResolver extends AbstractScalarTypeResolver
+{
+    
+}
